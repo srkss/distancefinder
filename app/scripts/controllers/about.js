467013056ab661327f89143ddeb63f772bdc28db
unknown <simhadrsr@HYD-SIMHADRSR-L.ds.ad.adp.com>
2015-09-03 16:44:09 +0530

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name distancefinderApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the distancefinderApp
+ */
+angular.module('distancefinderApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name distancefinderApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the distancefinderApp
+ */
+angular.module('distancefinderApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
